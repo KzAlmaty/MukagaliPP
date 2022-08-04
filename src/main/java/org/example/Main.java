f3ea0b9c2046e69476d980a0b9c9272604f4ff70
KzAlmaty <109860487+KzAlmaty@users.noreply.github.com>
2022-08-04 16:05:50 +0600

    Second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Здорово Ментор, как дела?");
+        System.out.println("Здорово Ментор, как дела? yoo");
+        System.out.println("Salam");
     }
 }
